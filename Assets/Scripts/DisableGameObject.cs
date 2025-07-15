8bb9940dfb66224cf47d05a4e50a254b353a1525
tycoon114 <sly114@naver.com>
2025-07-15 17:31:38 +0900

    효과음(펀치, 핑거스냅), BGM 추가( 타이틀,인게임, 승리,패배)

--- Assets/Scripts/DisableGameObject.cs
+++ Assets/Scripts/DisableGameObject.cs
@@ -4,6 +4,7 @@ public class DisableGameObject : MonoBehaviour
 {
     public void OnPunchingAnimationEnd()
     {
+        SoundManager.Instance.PlaySfx("ThanosPunch");
         gameObject.SetActive(false);
     }
 }
