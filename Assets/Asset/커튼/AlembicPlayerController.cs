4648947a87495a50beb74bdda9a683e99716f071
Cyphen <dbdxor@gmail.com>
2025-07-15 12:48:54 +0900

    [Resource] 커튼
    커튼

---
+++ Assets/Asset/커튼/AlembicPlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
+
+public class AlembicPlayerController : MonoBehaviour
+{
+    public AlembicStreamPlayer streamPlayer;
+    public float speed = 1f;
+    private float currentTime = 0f;
+
+    void Update()
+    {
+        if (streamPlayer != null)
+        {
+            currentTime += Time.deltaTime * speed;
+
+            // 루프 처리
+            if (currentTime > streamPlayer.Duration)
+                currentTime = 0f;
+
+            streamPlayer.UpdateImmediately(currentTime);
+        }
+    }
+}
